72beeb6051e8f9b5805871c053730dc5a680f640
Evan J Brunner <ej3@appitto.me>
2018-01-07 12:29:41 -0800

    test container volume mapping was static
    FIXED the volume mapping for the test container was static and
          specific to my machine.  Changed the mapping so that it
          will work on other people's systems.
    
    Signed-off-by: Evan J Brunner <ej3@appitto.me>

--- test.sh
+++ test.sh
@@ -3,16 +3,16 @@
 if [ -z "`docker ps -q -f name=dynago_test`" ]; then
 	echo Starting container for testing...
 	docker run \
-		-v /c/Users/ejbru/dev/appittome/dynago:/go/src/app \
+		-v `pwd`:/go/src/dynago \
 		-e AWS_ACCESS_KEY_ID=ABCD \
 		-e AWS_SECRET_ACCESS_KEY=123ABC \
 		-p 8000:8000 --name dynago_test --rm -d \
 		appittome/go_dynamo:latest
 fi
 #run test on init
-docker exec -t dynago_test go test ../../go/src/app
+docker exec -t dynago_test go test ../../go/src/dynago
 #run tests everytime something changes
 while inotifywait -qq --exclude \..*\.sw[a-z] -e modify -r .; do
 	echo running test suite...
-	docker exec -t dynago_test go test ../../go/src/app
+	docker exec -t dynago_test go test ../../go/src/dynago
 done
