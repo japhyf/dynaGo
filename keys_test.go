5076bf9b4db11c124aafbecc54b1229e1cf50ba6
Evan J Brunner <ej3@appitto.me>
2018-01-26 18:47:04 -0800

    Added tests for the Japhy KeyMaker spec
    Signed-off-by: Evan J Brunner <ej3@appitto.me>

---
+++ keys_test.go
@@ -0,0 +1,162 @@
+package dynaGo
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+var provider = credentials.StaticProvider{
+	credentials.Value{
+		AccessKeyID:     "id",
+		SecretAccessKey: "secret",
+		ProviderName:    "testProvider",
+	},
+}
+
+var (
+	tn = TableName(KeysTestUsrType)
+	in = "emailIndex"
+	fn = "email"
+	db = dynamodb.New(
+		session.New(
+			&aws.Config{
+				Credentials: credentials.NewCredentials(&provider),
+				Endpoint:    aws.String("http://localhost:8000"),
+				Region:      aws.String("us-east-1"),
+			}))
+)
+
+func TestMain(m *testing.M) {
+	//SETUP - try to build necessary tables
+	createTables()
+	os.Exit(m.Run())
+}
+func TestSecondaryIndexCreate(t *testing.T) {
+	if testTableHasIndex(tn, in, t) != nil {
+		deleteSecondaryIndex(KeysTestUsrType, in)
+		t.Errorf("Secondary Index %s exists when it should not!", in)
+	}
+	n, err := createSecondaryIndex(KeysTestUsrType, key{t: secGbl, pkn: fn})
+	if err != nil {
+		t.Errorf("could not create secondaryIndex %s:: %s", in, err)
+		t.FailNow()
+	}
+	if n != in {
+		t.Errorf("expected index name ('%s') not equal to returned name '%s'", in, n)
+		t.FailNow()
+	}
+	desc := testTableHasIndex(tn, in, t)
+	//Don't need to test name because found by name
+	matches := false
+	for _, a := range desc.KeySchema {
+		if aws.StringValue(a.AttributeName) == fn {
+			if aws.StringValue(a.KeyType) == "HASH" {
+				matches = true
+			}
+		}
+	}
+	if !matches {
+		t.Errorf("Index schema did not match intended value")
+	}
+}
+func TestSecondaryIndexDelete(t *testing.T) {
+	if testTableHasIndex(tn, in, t) == nil {
+		t.Errorf("%s table does not have index '%s' to delate!", tn, in)
+	}
+	if err := deleteSecondaryIndex(KeysTestUsrType, in); err != nil {
+		t.Errorf("Delete '%s' from '%s' failed:: %s", in, tn, err)
+	}
+	if testTableHasIndex(tn, in, t) != nil {
+		t.Errorf("Index '%s' continues to exist after deleted from table '%s'", in, tn)
+	}
+}
+
+func TestSecondaryIndexCreateKeyMaker(t *testing.T) {
+	em := "bob@vila.hmi"
+	km := CreateKeyMakerByName(KeysTestUsrType, fn)
+	key, err := km(em)
+	if err != nil {
+		t.Errorf("KeyMaker failed to construct key:: %s", err)
+	}
+	if key.t != secGbl {
+		t.Errorf("KeyMaker produced key of type '%d', expected '%d'", key.t, secGbl)
+	}
+	if key.pkn != fn {
+		t.Errorf("KeyMaker produced key with partition key '%s', expected '%s'", key.pkn, fn)
+	}
+	if key.rkn != "" {
+		t.Errorf("KeyMaker produced key with range key '%s', expected '%s'", key.rkn, fn)
+	}
+	if key.tbln != tn {
+		t.Errorf("KeyMaker produced key for table '%s', expected '%s'", key.tbln, tn)
+	}
+	if key.attr == nil || aws.StringValue(key.attr[fn].S) != em {
+		t.Error("KeyMaker produced incorrect attribute value map.")
+	}
+}
+
+// UTILITIES
+
+func testTableHasIndex(tn string, in string, t *testing.T) *dynamodb.GlobalSecondaryIndexDescription {
+	all, err := db.DescribeTable(&dynamodb.DescribeTableInput{TableName: aws.String(tn)})
+	if err != nil {
+		t.Errorf("Describe Table failed: %s", err)
+	}
+	for _, a := range all.Table.GlobalSecondaryIndexes {
+		if aws.StringValue(a.IndexName) == in {
+			return a
+		}
+	}
+	return nil
+}
+
+func testTableExists(name string, t *testing.T) bool {
+	all, err := db.ListTables(&dynamodb.ListTablesInput{})
+	if err != nil {
+		t.Errorf("ListTables failed: %s", err)
+	}
+	for _, a := range all.TableNames {
+		if aws.StringValue(a) == name {
+			return true
+		}
+	}
+	return false
+}
+
+// EXAMPLE TYPES
+
+type KeysTestUsr struct {
+	ID    string `dynaGo:"UserID,HASH"`
+	Phone string
+	Email string
+}
+
+type KeysTestMsg struct {
+	MID       string `dynaGo:"MessageID"`
+	RID       string `dynaGo:"RouteID,HASH"`
+	Timestamp int64  `dyanGo:",RANGE"`
+}
+
+// these are just easier to use than TypeOf() everywhere - reflect.Type
+var (
+	KeysTestUsrType = reflect.TypeOf((*KeysTestUsr)(nil)).Elem()
+	KeysTestMsgType = reflect.TypeOf((*KeysTestMsg)(nil)).Elem()
+)
+
+func createTables() {
+	fmt.Printf("CreateTable %s\n", TableName(KeysTestUsrType))
+	if err := CreateTable(db, KeysTestUsr{}, 1, 1); err != nil {
+		fmt.Printf("\t error occured while creating table: %s\n", err)
+	}
+	fmt.Printf("CreateTable %s\n", TableName(KeysTestMsgType))
+	if err := CreateTable(db, KeysTestMsg{}, 1, 1); err != nil {
+		fmt.Printf("\t error occured while creating table: %s\n", err)
+	}
+}
--- test.sh
+++ test.sh
@@ -9,10 +9,12 @@ if [ -z "`docker ps -q -f name=dynago_test`" ]; then
 		-p 8000:8000 --name dynago_test --rm -d \
 		appittome/go_dynamo:latest
 fi
-#run test on init
-docker exec -t dynago_test go test ../../go/src/dynago
 #run tests everytime something changes
 while inotifywait -qq --exclude \..*\.sw[a-z] -e modify -r .; do
 	echo running test suite...
-	docker exec -t dynago_test go test ../../go/src/dynago
+	if [ -z "$1" ]; then
+		docker exec -t dynago_test go test -run "$1" ../../go/src/dynago
+	else
+		docker exec -t dynago_test go test ../../go/src/dynago
+	fi
 done
